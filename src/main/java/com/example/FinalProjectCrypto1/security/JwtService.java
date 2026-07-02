195194c9fc4948c2b13607e10bca6633cbcabbdd
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-02 18:11:05 -0500

    feat: implement JWT authentication service and add JWT dependencies

--- src/main/java/com/example/FinalProjectCrypto1/security/JwtService.java
+++ src/main/java/com/example/FinalProjectCrypto1/security/JwtService.java
@@ -1,4 +1,57 @@
 package com.example.FinalProjectCrypto1.security;
 
+import com.example.FinalProjectCrypto1.model.seguridad.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+
+
+
+@Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "APRUEBAME_POR_FAVOR_INGENIERO";
+
+    public String generateToken(Usuario usuario) {
+
+        return Jwts.builder()
+                .setSubject(usuario.getUsuario())
+                .claim("role", usuario.getRol().getNombreRol())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
 }
