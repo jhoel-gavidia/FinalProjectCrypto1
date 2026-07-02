acb44683b7b77718bf9b311e9853caa6159790a5
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-07-02 18:02:17 -0500

    feat: Implement authentication service with JWT login logic

--- src/main/java/com/example/FinalProjectCrypto1/auth/AuthenticationService.java
+++ src/main/java/com/example/FinalProjectCrypto1/auth/AuthenticationService.java
@@ -1,4 +1,40 @@
 package com.example.FinalProjectCrypto1.auth;
 
+import com.example.FinalProjectCrypto1.dto.seguridad.JwtDto;
+import com.example.FinalProjectCrypto1.dto.seguridad.LoginDto;
+import com.example.FinalProjectCrypto1.model.seguridad.Usuario;
+import com.example.FinalProjectCrypto1.repository.seguridad.UsuarioRespository;
+import com.example.FinalProjectCrypto1.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    private final UsuarioRespository usuarioRespository;
+
+    public JwtDto login(LoginDto request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsuario(),
+                        request.getPassword()
+                )
+        );
+        Usuario usuario = usuarioRespository.findByUsuario(request.getUsuario())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        String token = jwtService.generateToken(usuario);
+
+        return new JwtDto(token);
+    }
+
+
 }
